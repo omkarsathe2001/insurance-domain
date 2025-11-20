bf2c3991efa4193f6f1578a421d85bae5f1a5d03
omkarsathe2001 <omkarsathe1927@gmail.com>
2025-11-20 18:25:38 +0530

    Added OpenAPI configuration for custom Swagger documentation

---
+++ src/main/java/com/insurance/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.insurance.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI insuranceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Insurance Backend API")
+                        .version("v1")
+                        .description("API documentation for Insurance Domain Application"));
+    }
+}
